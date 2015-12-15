3c19717dca6c5167d9432b3cd3eaacdb9f18d025
jimkinsey <jim.kinsey@springer.com>
2015-12-15 14:03:59 +0000

    Reimplement using regular expression; code is cleaner and shorter.

--- src/main/scala/com/github/jimkinsey/Mustache.scala
+++ src/main/scala/com/github/jimkinsey/Mustache.scala
@@ -1,18 +1,11 @@
 package com.github.jimkinsey
 
+import scala.util.matching.Regex.Match
+
 class Mustache {
   def render(template: String, context: Map[String, Any] = Map.empty): String = {
-    if (!template.contains("{{"))
-      template
-    else {
-      val variableIndex = template.indexOf("{{")
-      val remainder = template.substring(variableIndex + 2)
-      val prefix = template.substring(0, variableIndex)
-      val endOfVariableIndex: Int = remainder.indexOf("}}")
-      val key = remainder.substring(0, endOfVariableIndex)
-      val tail = remainder.substring(endOfVariableIndex + 2)
-
-      prefix + context.getOrElse(key, "") + render(tail, context)
-    }
+    """\{\{(.+?)\}\}""".r.findFirstMatchIn(template).map {
+      case m: Match => template.substring(0, m.start) + context.getOrElse(m.group(1), "") + render(template.substring(m.end), context)
+    }.getOrElse(template)
   }
 }
