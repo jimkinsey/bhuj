72675ad65afbd29753582f7dedd75e502475014a
Jim Kinsey <kinjacono@yahoo.co.uk>
2016-02-13 23:45:30 +0000

    Make text parser more readable using a pattern match.

--- src/main/scala/com/github/jimkinsey/mustache/parsing/TextParser.scala
+++ src/main/scala/com/github/jimkinsey/mustache/parsing/TextParser.scala
@@ -3,11 +3,12 @@ package com.github.jimkinsey.mustache.parsing
 import com.github.jimkinsey.mustache.components.Text
 
 private[mustache] object TextParser extends ComponentParser[Text] {
-  def parseResult(template: String)(implicit parserConfig: ParserConfig): Either[Any, Option[ParseResult[Text]]] = {
-    if (template == "") return Right(None)
-    val i = template.indexOf("{{")
-    if (i < 0) return Right(Some(ParseResult(Text(template), "")))
-    if (i > 0) return Right(Some(ParseResult(Text(template.substring(0, i)), template.substring(i))))
-    return Right(None)
+  def parseResult(template: String)(implicit parserConfig: ParserConfig) = {
+    template.indexOf("{{") match {
+      case _ if template == "" => Right(None)
+      case i if i < 0 => Right(Some(ParseResult(Text(template), "")))
+      case i if i > 0 => Right(Some(ParseResult(Text(template.substring(0, i)), template.substring(i))))
+      case _ => Right(None)
+    }
   }
 }
