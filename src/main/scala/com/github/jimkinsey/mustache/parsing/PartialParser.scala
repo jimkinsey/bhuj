0bd160b50c18c31b18f472c0fb196448148c8420
Jim Kinsey <kinjacono@yahoo.co.uk>
2016-02-13 23:47:59 +0000

    Reuse the Partial type alias for the render function passed to the partial parser.
    The non-aliased type is quite verbose and hard to read.

--- src/main/scala/com/github/jimkinsey/mustache/parsing/PartialParser.scala
+++ src/main/scala/com/github/jimkinsey/mustache/parsing/PartialParser.scala
@@ -1,13 +1,12 @@
 package com.github.jimkinsey.mustache.parsing
 
-import com.github.jimkinsey.mustache.Context
 import com.github.jimkinsey.mustache.components.Partial
 
 private[mustache] object PartialParser {
   case class TemplateNotFound(name: String)
 }
 
-private[mustache] class PartialParser(rendered: (String, Context) => Either[Any, String]) extends ComponentParser[Partial] {
+private[mustache] class PartialParser(rendered: Partial.Render) extends ComponentParser[Partial] {
   def parseResult(template: String)(implicit parserConfig: ParserConfig) = {
     Right(for {
       res <- s"\\{\\{>\\s*(.+?)\\s*\\}\\}".r.findPrefixMatchOf(template)
