7f44a2a1799d8eb891f5c017ddcc702f35f5eaf8
jimkinsey <kinjacono@yahoo.co.uk>
2016-02-22 17:16:01 +0000

    Fall back on looking for the template in the jar when not found at the explicit path.

--- src/main/scala/bhuj/partials/FilePartialLoader.scala
+++ src/main/scala/bhuj/partials/FilePartialLoader.scala
@@ -1,12 +1,17 @@
 package bhuj.partials
 
+import java.io.FileNotFoundException
+
 import scala.io.Source
 import scala.util.Try
 
 class FilePartialLoader(path: String) {
 
   def partial(name: String): Option[String] = {
-    Try(Source.fromFile(s"$path/$name.mustache").mkString).toOption
+    Try(Source.fromFile(s"$path/$name.mustache").mkString).recover {
+      case _: FileNotFoundException =>
+        Source.fromInputStream(getClass.getResourceAsStream(s"$path/$name.mustache")).mkString
+    }.toOption
   }
 
 }
