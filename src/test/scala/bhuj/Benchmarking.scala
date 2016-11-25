afa1cfd541e7b284e2a3ea1008867826972bb7c0
Jim Kinsey <jim.kinsey@springer.com>
2016-11-25 12:01:13 +0100

    Use a case class as the model when benchmarking

--- src/test/scala/bhuj/Benchmarking.scala
+++ src/test/scala/bhuj/Benchmarking.scala
@@ -4,34 +4,40 @@ object Benchmarking extends App {
   import MustacheBuilder._
   import bhuj.context.ContextImplicits._
 
-  val context: Context = Map(
-    "title" -> "Page Title",
-    "items" -> (1 to 100).map(i =>
-      Map(
-        "title" -> s"Item $i",
-        "content" -> "<p>This is a paragraph.</p>")
-    ),
-    "nav" -> Map(
-      "items" -> Seq(
-        Map("title" -> "Home", "link" -> "/"),
-        Map("title" -> "Contact", "link" -> "/contact"),
-        Map("title" -> "Away", "link" -> "http://whatever.com/", "tag" -> Some("external"))
+  case class NavItem(title: String, link: String, tag: Option[String] = None)
+  case class Nav(items: Seq[NavItem])
+  case class Item(title: String, content: String)
+  case class Page(title: String, items: Seq[Item], nav: Nav)
+
+  val context = Page(
+    title = "Page Title",
+    items = (1 to 100).map(i =>
+      Item(
+        title = s"Item $i",
+        content = "<p>This is a paragraph.</p>")
+      ),
+    nav = Nav(
+      items = Seq(
+        NavItem(title = "Home", link = "/"),
+        NavItem(title = "Contact", link = "/contact"),
+        NavItem(title = "Away", link = "http://whatever.com/", tag = Some("external"))
       )
     )
   )
+
   val runs = 1000
   val mustache = mustacheRenderer
     .withTemplatePath(getClass.getClassLoader.getResource("templates").getPath)
     .withCache
     .withHelpers("localised" -> ((template, rendered) => Right("LOCALISED!!!")))
-  def doRun =  mustache.renderTemplate("page", context)
+  def doRun() =  mustache.renderTemplate("page", context)
   val coldStart = System.currentTimeMillis()
   val coldRun = doRun
   val coldDuration = System.currentTimeMillis() - coldStart
   println(s"Cold run took ${coldDuration}ms")
   val start = System.currentTimeMillis()
   val results = (1 to runs).map(_ => {
-    doRun
+    doRun()
   })
   val duration = System.currentTimeMillis() - start
   val avg = duration.toFloat / runs.toFloat
